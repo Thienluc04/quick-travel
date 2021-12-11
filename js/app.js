c1917723994dc7fe7b3fd81af7a36c331aec0879
Luc <nguyenthienluc2004@gmail.com>
2021-12-11 10:00:05 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const headerMain = document.querySelector(".menu");
+
+window.onscroll = function () {
+  showProduct();
+};
+
+function showProduct() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  }
+}
